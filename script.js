b8672d63fba488d641c98ca0a8b0ff58225111d4
PinkSpaceBee <minaary@mailfence.com>
2021-12-08 11:49:14 +0200

    playing with storing objects in localStorage

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ function createGarden1() {
     storePlant();
 
 }
-
 createGarden1();
 
 function createGarden2() {
@@ -54,5 +53,100 @@ function createGarden2() {
 
     storePlant();
 }
-
 createGarden2();
+
+function createGarden3() {
+    const garden = document.querySelector('#garden-3');
+    const textInput = document.querySelector('#g3-text-input');
+    const numInput = document.querySelector('#g3-num-input');
+    const addBtn = document.querySelector('#g3-add-btn');
+    const removeBtn = document.querySelector('#g3-rm-btn');
+    
+    function PlantObj(name, quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    const arr = [];
+    const plant = document.createElement('div');
+    let span;
+
+    addBtn.addEventListener('click', () => {
+        arr.push(new PlantObj(textInput.value, numInput.value));
+        localStorage.setItem('plant', JSON.stringify(arr[arr.length-1]));
+        storePlant();
+
+        console.log(`check ${localStorage.getItem('plant')}`);
+    });
+
+    removeBtn.addEventListener('click', () => {
+        localStorage.removeItem('plant');
+        plant.remove();
+        console.log(`check ${localStorage.getItem('plant')}`);
+    });
+
+    function storePlant() {
+        //const parsed = JSON.parse(localStorage.getItem(' '))
+        if (localStorage.getItem('plant')) {
+
+            console.dir(arr);
+            console.log(arr[arr.length - 1]);
+
+            //for (const [key, value] of Object.entries(arr[arr.length - 1])) {
+            for (const [key, value] of Object.entries(JSON.parse(localStorage.getItem('plant')))) {
+                span = document.createElement('span');
+                plant.appendChild(span);
+                span.textContent = `${key}: ${value} `;
+            }
+            garden.appendChild(plant);
+        }
+    }
+
+    //storePlant();
+
+}
+createGarden3();
+
+function test() {
+    const arr = [{q: 'q', w: 'w'}];
+    const body = document.querySelector('body');
+    const div = document.createElement('div');
+
+    const buttonA = body.appendChild(document.createElement('button'));
+    buttonA.textContent = 'press me';
+    const buttonR = body.appendChild(document.createElement('button'));
+    buttonR.textContent = `I'M A DESTRIYER OF WORLDS`;
+
+    buttonA.addEventListener('click', () => {
+        //localStorage.setItem('div', arr[0].q); works
+        //localStorage.setItem('div', strQ); works
+        localStorage.setItem('div', JSON.stringify(arr[0]));
+        storeObj();
+        console.log(typeof arr[0].q);
+        console.log(localStorage.getItem('div'))
+    });
+    buttonR.addEventListener('click', () => {
+        removeObj();
+    });
+
+    const strQ = arr[0].q;
+    const str = 'str';
+
+    function storeObj() {
+        if (localStorage.getItem('div')) {
+            //localStorage.setItem('div', str);
+            body.appendChild(div);
+            div.textContent = localStorage.getItem('div');
+            console.log(localStorage.getItem(arr[0]));
+        }
+    }
+
+    function removeObj() {
+        localStorage.removeItem('div');
+        div.remove();
+    }
+
+    storeObj();
+}
+
+test();
